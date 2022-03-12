c2362798aa4becff0a373b39adcb42f2bd0e3af0
newbee1939 <hideto3970@gmail.com>
2022-03-12 21:19:38 +0900

    Webサイトにリクエストを送って情報を取得するプログラムを作成

--- cmd/net/main.go
+++ cmd/net/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// I/O(入出力)関連の便利なパッケージを提供するパッケージ
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+	// アクセスするURL
+	p := "https://engilaboo.com"
+	// 指定のアドレスにアクセスする
+	re, er := http.Get(p)
+	if er != nil {
+		// プログラムを強制終了する
+		panic(er)
+	}
+	// deferでは「プログラムを終了する際に必ず実行する処理」を指定できる
+	// ReadCloserの解放処理
+	defer re.Body.Close()
+
+	// Getで得られたResponseからコンテンツを取り出す
+	s, er := ioutil.ReadAll(re.Body)
+	if er != nil {
+		panic(er)
+	}
+
+	// 出力
+	fmt.Println(string(s))
 }
